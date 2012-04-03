7767401a4519631f167a86b76e5deadd885b7460
Mark Kennard <komcomputers@gmail.com>
2012-04-03 13:42:37 +1200

    Branding and build script from old kernel

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-make apollo_rev_02_android_defconfig
-make -j8
-cd modules/vibetonz
-make KDIR=../../
-cd ../fm_si4709
-make KDIR=../../
+
+echo "Generating initramfs.cpio"
+cd ..
+./initramfs.sh
+cd Kernel
+
+if [ -z $1 ]; then
+	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
+else
+	echo "Setting kernel name to ($1)"
+	export KBUILD_BUILD_VERSION=$1
+fi
+
+echo "Compiling the kernel"
+make -j2
+
+echo "Done"
