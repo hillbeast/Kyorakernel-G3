b33922bc4c30baf1887ae84c1825777ac7340aaa
Mark Kennard <komcomputers@gmail.com>
2012-04-06 21:03:01 +1200

    Added SG3 partition table to MTD (but I don't think samsung.h is used)

--- drivers/mtd/onenand/s5p6442.c
+++ drivers/mtd/onenand/s5p6442.c
@@ -85,48 +85,7 @@ struct onenand_info {
 	struct onenand_chip		onenand;
 };
 
-struct mtd_partition s3c_partition_info[] = {
-	{
-		.name		= "bootloader",
-		.offset		= (0),          /* for bootloader */
-		.size			= (512*SZ_1K),
-		.mask_flags	= MTD_CAP_NANDFLASH,
-	},
-	{
-		.name		= "param",
-		.offset		= MTDPART_OFS_APPEND,
-		.size			= (1*SZ_1M),
-		//.mask_flags	= MTD_CAP_NANDFLASH,
-	},
-	{
-		.name		= "kernel",
-		.offset		= MTDPART_OFS_APPEND,
-		.size			=(6*SZ_1M),
-	},
-//	{
-//		.name		= "ramdisk",
-//		.offset		= MTDPART_OFS_APPEND,
-//		.size		= (3*SZ_1M),
-//	},
-	{
-		.name		= "system",
-		.offset		= MTDPART_OFS_APPEND,
-		.size			= (100*SZ_1M),
-	},
-	{
-		.name		= "cache",
-		.offset		= MTDPART_OFS_APPEND,
-		.size			= (80*SZ_1M),
-		//.size		= (200*SZ_1M),		// iozone test
-	},
-	{
-		.name		= "userdata",
-		.offset		= MTDPART_OFS_APPEND,
-		.size			= (50*SZ_1M),
-//		.size               = MTDPART_SIZ_FULL,
-	}
-
-};
+#include "samsung_apollo.h"
 
 /* Default Flex-OneNAND boundary and lock respectively */
 static int flex_bdry[MAX_DIES * 2] = { -1, 0, -1, 0 };
