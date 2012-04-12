7314e67ad19c079fce28edfaf2e1600e50f41d1d
Mark Kennard <komcomputers@gmail.com>
2012-04-12 14:22:32 +1200

    This file doesn't need to be here

--- include/linux/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version Test-Preempt-032 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "Kyorakernel_#Test-Preempt-032 PREEMPT Thu Apr 12 00:48:31 NZST 2"
-#define LINUX_COMPILE_TIME "00:48:31"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "hillbeast-virtual-machine"
-#define LINUX_COMPILE_DOMAIN
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
